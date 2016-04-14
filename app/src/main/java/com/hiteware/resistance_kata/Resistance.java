7b121bf5f7709feca77a15404c27d583d8f4b0d2
Jason Hite <HiteJasonC@JohnDeere.com>
2016-04-14 18:36:30 -0400

    Returns the correct sum it expects from resistance

---
+++ app/src/main/java/com/hiteware/resistance_kata/Resistance.java
@@ -0,0 +1,13 @@
+package com.hiteware.resistance_kata;
+
+/**
+ * Created on 4/14/16.
+ */
+public class Resistance {
+    public Resistance(Integer[][] grid) {
+    }
+
+    public int leastResistanceCount() {
+        return 42;
+    }
+}
--- app/src/test/java/com/hiteware/resistance_kata/ResistanceUnitTest.java
+++ app/src/test/java/com/hiteware/resistance_kata/ResistanceUnitTest.java
@@ -9,7 +9,11 @@ import static org.junit.Assert.*;
  */
 public class ResistanceUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void sums_up_resistance_for_one_row_sum_less_than_50() {
+        int rows = 1;
+        int columns = 5;
+        Integer[][] grid = new Integer[rows][columns];
+        Resistance resistance = new Resistance(grid);
+        assertEquals(resistance.leastResistanceCount(), 42);
     }
 }
