92c26bc739140a324b5b95ea3f450eef799e8692
Jason Hite <HiteJasonC@JohnDeere.com>
2016-04-14 22:18:27 -0400

    Can wrap around going up

--- app/src/main/java/com/hiteware/resistance_kata/Path.java
+++ app/src/main/java/com/hiteware/resistance_kata/Path.java
@@ -7,7 +7,12 @@ import java.util.List;
  * Created on 4/14/16.
  */
 public class Path {
+    private final Integer[][] resistence_grid;
     private List<Integer> positions = new ArrayList<>();
+
+    public Path(Integer[][] grid) {
+        resistence_grid = grid;
+    }
 
     public void setStartPosition(int row) {
         positions.add(row);
@@ -18,6 +23,8 @@ public class Path {
     }
 
     public void moveUp() {
-        positions.add(4);
+        int lastPosition = positions.get(positions.size()-1);
+        int nextPosition = (lastPosition == 1) ? resistence_grid.length: lastPosition-1;
+        positions.add(nextPosition);
     }
 }
--- app/src/test/java/com/hiteware/resistance_kata/PathUnitTest.java
+++ app/src/test/java/com/hiteware/resistance_kata/PathUnitTest.java
@@ -1,5 +1,6 @@
 package com.hiteware.resistance_kata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -12,9 +13,25 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created on 4/14/16.
  */
 public class PathUnitTest {
+
+    Integer[][] grid_five_tall = new Integer[5][2];
+
+    @Before
+    public void setup() {
+        initialize_grid_five_tall();
+    }
+
+    public void initialize_grid_five_tall() {
+        grid_five_tall[0][0] = 0; grid_five_tall[0][1] = 0;
+        grid_five_tall[1][0] = 0; grid_five_tall[1][1] = 0;
+        grid_five_tall[2][0] = 0; grid_five_tall[2][1] = 0;
+        grid_five_tall[3][0] = 0; grid_five_tall[3][1] = 0;
+        grid_five_tall[4][0] = 0; grid_five_tall[4][1] = 0;
+    }
+
     @Test
     public void can_store_start_position_and_recall_position() {
-        Path path = new Path();
+        Path path = new Path(grid_five_tall);
         path.setStartPosition(3);
         List<Integer> positions = new ArrayList<>();
         positions.add(3);
@@ -23,12 +40,23 @@ public class PathUnitTest {
 
     @Test
     public void can_store_a_move_up_and_recall_positions() {
-        Path path = new Path();
+        Path path = new Path(grid_five_tall);
         path.setStartPosition(5);
         path.moveUp();
         List<Integer> positions = new ArrayList<>();
         positions.add(5);
         positions.add(4);
+        assertThat(path.recallPositions(), equalTo(positions));
+    }
+
+    @Test
+    public void can_wrap_around_the_top_to_bottom_position() {
+        Path path = new Path(grid_five_tall);
+        path.setStartPosition(1);
+        path.moveUp();
+        List<Integer> positions = new ArrayList<>();
+        positions.add(1);
+        positions.add(5);
         assertThat(path.recallPositions(), equalTo(positions));
     }
 }
